dcd1498fee0b58f4b3f37c9c70c31a62bc2ad9f4
Javier <jahuba85@hotmail.com>
2015-12-24 20:32:13 -0500

    render parcial y edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,8 +31,17 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
   #put /articles/:id
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   private
