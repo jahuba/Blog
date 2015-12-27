9a98425fa79453bd0e87f75be3e318e9677d9a52
Javier <jahuba85@hotmail.com>
2015-12-27 18:22:29 -0500

    Agregada foranea de usuario a los articulos

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   # la tabla
   # los campos
   # metodos
+  belongs_to :user
   validates :title,  presence: true, uniqueness: true
   validates :body, presence: true, length: {minimum: 20}
 end
